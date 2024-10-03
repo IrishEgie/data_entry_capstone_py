b1f1526585828c502ffa5bc2e60e4ec0a850b5cb
IrishEgie <irishegie.arao@ssu.edu.ph>
2024-10-03 14:54:41 +0800

    update: reorganized code base

--- form.py
+++ form.py
@@ -4,34 +4,35 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
 import os
 
-
 options = Options()
-options.set_preference("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0")
 
-driver = webdriver.Firefox(options=options)
-driver.get(os.getenv("FORM_LINK"))
+def open_driver():
+    options.set_preference("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0")
+    global driver
+    driver= webdriver.Firefox(options=options)
 
-address = driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
-price = driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
-prop = driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
-link = driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[1]/div/div[1]/input')
-submit =  driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div')
+def form_interaction(f_rentals_address_list,
+f_rentals_price_list,
+f_rentals_properties_list,
+f_rentals_link_list):
 
+    driver.get(os.getenv("FORM_LINK"))
 
-def form_interaction(): 
+    address = driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    price = driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    prop = driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    link = driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    submit =  driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div')
+ 
     # Interact with the form
-    address.send_keys("Test")
-    price.send_keys("Test")
-    prop.send_keys("Test")
-    link.send_keys("Test")
+    address.send_keys(f_rentals_address_list)
+    price.send_keys(f_rentals_price_list)
+    prop.send_keys(f_rentals_properties_list)
+    link.send_keys(f_rentals_link_list)
     submit.send_keys(Keys.RETURN)
+    driver.close()
 
-form_interaction()
+def form_quit():
+    # # Close the driver
+    driver.quit()
 
-# # Close the driver
-driver.close()
-
-#     f_rentals_address_list,
-# f_rentals_price_list,
-# f_rentals_properties_list,
-# f_rentals_link_list
--- main.py
+++ main.py
@@ -1,64 +1,26 @@
-from bs4 import BeautifulSoup
-import os
-import requests as rq
-import re  # For regex operations
+import soup_fetch 
 import form
 
-response = rq.get(os.getenv("ZILLOW_SCRAPE_LINK"))
 
-soup = BeautifulSoup(response.text, "html.parser")
+f_rentals_address_list = soup_fetch.soup_fetch()[0]
+f_rentals_price_list = soup_fetch.soup_fetch()[1]
+f_rentals_properties_list = soup_fetch.soup_fetch()[2]
+f_rentals_link_list = soup_fetch.soup_fetch()[3]
 
+# print(len(f_rentals_address_list))
+# print(len(f_rentals_price_list))
+# print(len(f_rentals_properties_list))
+# print(len(f_rentals_link_list))
 
-# ----------------------------- Address ---------------------------- #
-# Find all rental tiles
-rentals_address_soup = soup.find_all(name="a", class_="StyledPropertyCardDataArea-anchor")
-rentals_properties_list = [details.getText().split() for details in rentals_address_soup]
+# form.open_driver()
 
-# Format the rentals addresses
-f_rentals_address_list = []
-for rental in rentals_properties_list:
-    formatted = " ".join(rental)
-    f_rentals_address_list.append(formatted)
-# print(f_rentals_address_list) # For Debugging
-
-
-# ----------------------------- Price ---------------------------- #
-rentals_price_soup = soup.find_all(name="span", class_= "PropertyCardWrapper__StyledPriceLine")
-rentals_price_list = [price.getText() for price in rentals_price_soup]
-
-f_rentals_price_list = []
-for price_tag in rentals_price_list:
-    str_price = re.sub('[^0-9]','', price_tag)
-    if len(str_price) > 4:  # Ensure price is not more than 4 digits
-        str_price = str_price[:4]  # Take only the first 4 digits
-    f_price = int(str_price)
-    f_rentals_price_list.append(f_price)
-# print(f_rentals_price_list) # For Debugging
-
-
-# ----------------------------- Properties ---------------------------- #
-rentals_properties_soup = soup.find_all(name="ul", class_="StyledPropertyCardHomeDetailsList")
-rentals_properties_list = [prop.getText().split() for prop in rentals_properties_soup]
-
-f_rentals_properties_list = []
-for prop in rentals_properties_list:
-    # Filter out empty strings
-    if prop:  # Checks if the list 'prop' is not empty
-        formatted = " ".join(prop)
-        f_rentals_properties_list.append(formatted)
-f_rentals_properties_list = [entry for entry in f_rentals_properties_list if entry.strip()] # Filter out any empty strings from the final list (just in case)
-# print(f_rentals_properties_list)
-
-
-# ----------------------------- Links ---------------------------- #
-rentals_link_soup = soup.find_all(name="a", href=True, class_= "StyledPropertyCardDataArea-anchor")
-
-f_rentals_link_list = []
-for link in rentals_link_soup:
-    f_rentals_link_list.append(link['href'])
-print(f_rentals_link_list)
-
-
-
-
+f_rentals_dict = {}
+for index in range(len(f_rentals_address_list)):
+    form.form_interaction(
+    f_rentals_address_list[index],
+    f_rentals_price_list[index],
+    f_rentals_properties_list[index],
+    f_rentals_link_list[index]
+    )
+# form.form_quit()
 
